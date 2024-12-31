cb1fde225fc1a7e88429475cec464f625036d88b
wendy696905 <w696905@gmail.com>
2024-12-31 21:24:54 +0800

    helloFastApi

--- app/api/helloNextJs/route.ts
+++ app/api/helloNextJs/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  return NextResponse.json({ message: "Hello from Next.js 14" });
-}
+  const response = await fetch('http://127.0.0.1:8000/api/helloFastApi');
+  const data = await response.json();
+  return NextResponse.json({ message: "Hello from Next.js 14.Hahaha", data: data });
+ }
--- next.config.js
+++ next.config.js
@@ -3,10 +3,10 @@ const nextConfig = {
   rewrites: async () => {
     return [
       {
-        source: "/api/py/:path*",
+        source: "/api/:path*",
         destination:
           process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/py/:path*"
+            ? "http://127.0.0.1:8000/api/:path*"
             : "/api/",
       },
       {
